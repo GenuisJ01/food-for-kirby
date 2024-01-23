0f566cef62a15da3e331752d3dc558196208f869
GenuisJ01 <c.altinay01@gmail.com>
2024-01-23 11:54:00 +0000

    Create new get for root of backend, notes number of profiles

--- backend/app.js
+++ backend/app.js
@@ -13,4 +13,8 @@ app.use("/profiles", express.json())
 
 app.use(logger);
 
+app.get('/', (req, res) => {
+    res.status(200).send(`Kirby would like to have a word... He wants to show you this data! There are currently ${profiles.length} profile(s) made!`)
+})
+
 module.exports = {app, port};
