3f1089a5e2f6b0918156fe7eb299aec675ca4f02
GenuisJ01 <c.altinay01@gmail.com>
2024-01-23 12:06:41 +0000

    Make sure that the plural words are used when needed for qol, and now has a clues database (empty)

--- backend/app.js
+++ backend/app.js
@@ -4,17 +4,29 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const logger = require("./logger");
-const profiles = require("./profiles.json");
+const profiles = require("./data/profiles.json");
+const clues = require("./data/clues.json");
 
 const app = express();
 const port = process.env.PORT;
 app.use(cors());
-app.use("/profiles", express.json())
+app.use("/profiles", express.json());
+app.use("/clues", express.json());
 
 app.use(logger);
 
 app.get('/', (req, res) => {
-    res.status(200).send(`Kirby would like to have a word... He wants to show you this data! There are currently ${profiles.length} profile(s) made!`)
+    if (profiles.length == 1 && clues.length == 1) {
+        res.status(200).send(`Kirby would like to have a word... He wants to show you this data! There are currently ${profiles.length} profile made, and a total of ${clues.length} clue to pick from!`);
+    } else if (profiles.length == 1 && clues.length > 1) {
+        res.status(200).send(`Kirby would like to have a word... He wants to show you this data! There are currently ${profiles.length} profile made, and a total of ${clues.length} clues to pick from!`)
+    } else if (profiles.length > 1 && clues.length == 1) {
+        res.status(200).send(`Kirby would like to have a word... He wants to show you this data! There are currently ${profiles.length} profiles made, and a total of ${clues.length} clue to pick from!`)
+    } else if (profiles.length > 1 && clues.length > 1) {
+        res.status(200).send(`Kirby would like to have a word... He wants to show you this data! There are currently ${profiles.length} profiles made, and a total of ${clues.length} clues to pick from!`);
+    } else {
+        res.status(406).send(`Kirby couldn't make it work... The profiles or clues database has an error, and cannot be brought up.`)
+    }
 })
 
 module.exports = {app, port};
