0deb131398958c504edcdea29edd21b7c1883e5f
Rahma Ali <rahma.ma078@gmail.com>
2024-01-23 14:07:25 +0000

    Added get and post requests, added to profile.json and clues.json.

--- backend/app.js
+++ backend/app.js
@@ -13,4 +13,28 @@ app.use("/profiles", express.json())
 
 app.use(logger);
 
+app.post('/profiles',(req,res)=>{
+    const profile=req.body;
+    profiles.push(profile)
+    res.status(201).send(profile)
+})
+
+app.get('/',(req,res)=>{
+    res.status(200).send("Hello!")
+})
+
+app.get('/profiles',(req,res)=>{
+    res.send(profiles)
+})
+
+app.get('/profiles/:id',(req,res)=>{
+    const id = req.params.id;
+    const profile = profiles.find((profile) => profile.id == id);
+    if (profile==undefined){
+        res.status(404).send("This user ID does not exist.");
+    } else {
+        res.send(profile);
+    }
+});
+
 module.exports = {app, port};
