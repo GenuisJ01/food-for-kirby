1d1cb4a82d5943062c790a9acbf631017f4543d9
APrudente15 <110691505+APrudente15@users.noreply.github.com>
2024-01-24 12:08:07 +0000

    added more sections and  I have been trying to fix layout issues with three different images

--- frontend/javascript/puzzle1.js
+++ frontend/javascript/puzzle1.js
@@ -1,1 +1,28 @@
 // Main javascript file for all behaviour in puzzle1.html
+const openModalButtons = document.querySelectorAll('[data-model-toggle]')
+const closeModalButton = document.querySelector('[data-close-btn]')
+const overlay = document.getElementById('overlay')
+
+function openModal(modal) {
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal')
+        closeModal(modal)
+    })
+})
