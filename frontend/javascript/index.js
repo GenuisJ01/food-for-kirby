3150ee4bf1ee9de9bd921ce72fbb6cf663486a91
APrudente15 <110691505+APrudente15@users.noreply.github.com>
2024-01-24 17:05:56 +0000

    if this new commit is applied, my commit will show that I have created a popup page in the index html file, along with its functionalities n the index js file

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,1 +1,14 @@
 // Main javascript file for all behaviour in index.html
+
+const popup = document.getElementById("topicsPopUp");
+
+function openPopUp(){
+    topicsPopUp.classlist.add("open-popup");
+}
+
+function closePopUp(){
+    topicsPopUp.classlist.remove("open-popup");
+}
+
+console.log(closePopUp)
+console.log(openPopUp)
